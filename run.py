b2f8921c1472a7872bb3deb05b1fefe08e52d9be
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-08-28 22:23:31 +0100

    Add user request loop to repeat reuqest fgor ddata until data provided is valid

--- run.py
+++ run.py
@@ -27,16 +27,17 @@ def get_sales_data():
     ''''
     Get the sales data from the user
     '''
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers separated by a comma.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers separated by a comma.")
-    print("Example: 10,20,30,40,50,60\n")
-
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    if validate_sales_data(sales_data):
-        return sales_data
-
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+        if validate_sales_data(sales_data):
+            print("Data is valid!\n")
+            break
+    return sales_data
 
 
 def validate_sales_data(values):
@@ -51,10 +52,11 @@ def validate_sales_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
 
 
-
-
-get_sales_data()
+data = get_sales_data()
