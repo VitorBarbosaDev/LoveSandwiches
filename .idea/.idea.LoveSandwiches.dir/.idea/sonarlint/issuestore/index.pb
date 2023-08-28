67f39a25bbc8320fa9fce4d8b6567e1a14d5cd0e
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-08-28 21:49:47 +0100

    Wire upAPIs and confirm access top google sheet

---
+++ .idea/.idea.LoveSandwiches.dir/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+8
+index.js,a\7\a77b15c0d64cf6111e025645e562faf8ccabb9d7
+:
+
+creds.json,f\b\fb6220478aaba649aac37271a1d7c6317abc03a6
+|
+L.idea/.idea.LoveSandwiches.dir/.idea/sonarlint/securityhotspotstore/index.pb,9\c\9c0c4af45413eb6ab0cd9d696309665195897ca4
+r
+B.idea/.idea.LoveSandwiches.dir/.idea/sonarlint/issuestore/index.pb,d\9\d9ecfe222aeb32b633a2c86a6fb0e27a3c1e3a21
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
--- run.py
+++ run.py
@@ -1,3 +1,38 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Define the scope for Google APIs
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# Load credentials from the service account file
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+
+# Authorize and connect to Google Sheets
+GSPREAD = gspread.authorize(SCOPED_CREDS)
+
+# Open the Google Sheet
+SHEET = GSPREAD.open('love_sandwiches')
+
+
+# Access the 'Sales' worksheet
+sales = SHEET.worksheet('sales')
+
+# Retrieve all values from the 'Sales' worksheet
+data = sales.get_all_values()
+
+# Print the data
+print(data)
+
+
+
+
